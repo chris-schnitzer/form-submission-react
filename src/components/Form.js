7ef5c9f66549c0baad85d38015ad40fe9442c507
chris-schnitzer <62219119+chris-schnitzer@users.noreply.github.com>
2023-02-24 19:24:35 +0000

    Get inputted values of form to display below

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,37 @@
+import { useState } from 'react';
+
 export default function  Form() {
+	
+	const [heightFeet, setHeightFeet] = useState('');
+	const [heightInches, setHeightInches] = useState('');
+	const [weight, setWeight] = useState('');
+	const [exp, setExp] = useState('');
+	const [message, setMessage] = useState('');
+
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		setHeightFeet('');
+		setHeightInches('');
+		setWeight('');
+		setExp('');
+		setMessage(`Your height is ${heightFeet} foot ${heightInches} inches, 
+		your weight is ${weight}kgs and you are a ${exp}`);
+	}
 	return (
 		<>
-			<form>	
+			<form onSubmit={handleSubmit}>	
 					<label>Height</label>
 					
 					<div className="height-items">
-						<select className="sm-item">
+						<select 
+							className="sm-item"
+							value={heightFeet}
+							onChange={function(event) {
+								setHeightFeet(event.target.value);
+							}}
+
+						>
 							<option value="">Feet</option>
 							<option value="1">1</option>
 							<option value="2">2</option>
@@ -15,7 +41,13 @@ export default function  Form() {
 							<option value="6">6</option>
 							<option value="7">7</option>
 						</select>
-						<select className="sm-item">
+						<select 
+							className="sm-item"
+							value={heightInches}
+							onChange={function(event) {
+								setHeightInches(event.target.value);
+							}}
+						>
 							<option value="">Inches</option>
 							<option value="1">1</option>
 							<option value="2">2</option>
@@ -31,17 +63,35 @@ export default function  Form() {
 						</select>
 					</div>
 					<label>Weight</label>	
-					<input type="text" name="weight" placeholder="Weight in kilograms" />
+					<input 
+						type="text" 
+						name="weight" 
+						placeholder="Weight in kilograms" 
+						value={weight}
+						onChange={function(event) {
+							setWeight(event.target.value);
+						}}
+					/>
+					
 					<label>Experience</label>
-					<select className="full-length">
-						<option value="">Beginner</option>
-						<option value="">Intermediate</option>
-						<option value="">Pro</option>
+					<select 
+						className="full-length"
+						value={exp}
+						onChange={function(event) {
+							setExp(event.target.value);
+						}}
+					>
+						<option>Beginner</option>
+						<option>Intermediate</option>
+						<option>Pro</option>
 					</select>
+
+
 					<button 
-						type="button">
+						type="button" onClick={handleSubmit}>
 					Continue
 					</button>
+					<h4>{message}</h4>
 				</form>
 		</>
 	)
